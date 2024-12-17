32bdab41ed66e5429cbcfdcca5805100292b95d9
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2024-12-17 12:46:48 +0100

    wip

---
+++ Sources/CoenttbWebSyndication/API/Syndication.API.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  coenttb-web
+//
+//  Created by Coen ten Thije Boonkkamp on 17/12/2024.
+//
+
+import Foundation
+
+public enum API: Equatable, Sendable {
+    case image(String)
+}
