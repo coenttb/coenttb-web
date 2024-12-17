20583239c5ba2d1a4d3a7208c36201105de2759f
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2024-12-17 12:49:24 +0100

    wip

--- Sources/CoenttbWebSyndication/API/Syndication.API.swift
+++ Sources/CoenttbWebSyndication/API/Syndication.API.swift
@@ -5,8 +5,29 @@
 //  Created by Coen ten Thije Boonkkamp on 17/12/2024.
 //
 
+import Dependencies
 import Foundation
+import Languages
+import URLRouting
+import MacroCodableKit
 
 public enum API: Equatable, Sendable {
     case image(String)
+}
+extension CoenttbWebSyndication.API {
+    public struct Router: ParserPrinter, Sendable {
+        
+        public init(){}
+        
+        public var body: some URLRouting.Router<CoenttbWebSyndication.API> {
+            OneOf {
+                URLRouting.Route(.case(CoenttbWebSyndication.API.image)) {
+                    Path {
+                        "image"
+                        Parse(.string)
+                    }
+                }
+            }
+        }
+    }
 }
