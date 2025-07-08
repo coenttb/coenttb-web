34e9d93fd0b873afc45cda13cfe27ac6edfdf235
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-08 13:24:58 +0200

    wip

--- Sources/Coenttb Web HTML/CustomStringConvertible.swift
+++ Sources/Coenttb Web HTML/CustomStringConvertible.swift
@@ -9,13 +9,19 @@ import Foundation
 import CoenttbHTML
 
 public extension String.StringInterpolation {
-    mutating func appendInterpolation(html value: some HTML) {        
+    mutating func appendInterpolation(html value: some HTML) {
         let bytes: ContiguousArray<UInt8> = value.render()
-        let string: String = String(decoding: bytes, as: UTF8.self)
-            .replacingOccurrences(of: "\"", with: "\\\"")
-            .replacingOccurrences(of: "\n", with: "\\n")
-            .replacingOccurrences(of: "\r", with: "")
+        let htmlString: String = String(decoding: bytes, as: UTF8.self)
         
-        appendLiteral(string)
+        // Escape characters that would break template literals
+        let escapedString = htmlString
+            .replacingOccurrences(of: "\\", with: "\\\\")   // Escape backslashes first (must be first!)
+            .replacingOccurrences(of: "`", with: "\\`")     // Escape backticks
+            .replacingOccurrences(of: "${", with: "\\${")   // Escape template literal expressions
+            .replacingOccurrences(of: "\u{2028}", with: "\\u2028") // Escape line separator
+            .replacingOccurrences(of: "\u{2029}", with: "\\u2029") // Escape paragraph separator
+        
+        // Wrap in backticks to create a template literal
+        appendLiteral("`\(escapedString)`")
     }
 }
