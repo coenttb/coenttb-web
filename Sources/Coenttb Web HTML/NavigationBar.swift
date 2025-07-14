ae8363808bbe46c8f4b8410930310d0fdad1fac0
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-14 12:46:03 +0200

    fix

--- Sources/Coenttb Web HTML/NavigationBar.swift
+++ Sources/Coenttb Web HTML/NavigationBar.swift
@@ -89,6 +89,7 @@ public struct NavigationBar<
                 .id("menu-icon")
                 .attribute("for", "menu-checkbox")
                 .cursor(.pointer)
+                .display(.block, media: .mobile)
                 .display(Display.none, media: .desktop)
                 .inlineStyle("user-select", "none")
         }
@@ -99,10 +100,6 @@ public struct NavigationBar<
                     HTMLForEach(-1...1) { index in
                         Bar(index: index)
                     }
-//                    .size(
-//                        width: .px(24),
-//                        height: .px(3)
-//                    )
                     .size(
                         .double(width: .px(24), height: .px(3))
                     )
